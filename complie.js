b40826f26615a266f50b58a49921d81cbef705a7
wei-elementary <haoyifang@doctor.com>
2021-03-18 15:43:25 +0800

    add watcher and dep to finish all update value of vue's data

--- complie.js
+++ complie.js
@@ -10,16 +10,14 @@ class Compiler {
     }
   }
 
-	
   complie(el) {
-
-		// 遍历el树
+    // 遍历el树
     const childNodes = el.childNodes;
     Array.from(childNodes).forEach((node) => {
-			// console.log(this.isElement(node));
+      // console.log(this.isElement(node));
       if (this.isElement(node)) {
         // console.log("编译元素" + node.nodeName);
-				this.complieElement(node)
+        this.complieElement(node);
       } else if (this.isInter(node)) {
         // console.log("编译插值绑定" + node.textContent);
         this.compileText(node);
@@ -36,39 +34,62 @@ class Compiler {
   }
 
   isInter(node) {
-		// console.log(node.textContent);
+    // console.log(node.textContent);
     // 正则()分组
     return node.nodeType === 3 && /\{\{(.*)\}\}/.test(node.textContent);
   }
 
   compileText(node) {
-    node.textContent = this.$vm[RegExp.$1];
+    // node.textContent = this.$vm[RegExp.$1];
+    this.update(node, RegExp.$1, "text");
   }
 
-	complieElement(node){
-		//节点是元素
-		//遍历属性列表
-		const nodeAttrs = node.attributes
-		Array.from(nodeAttrs).forEach(attr=>{
-			// 以k-xx="oo"为例
-			const attrName = attr.name // k-xx
-			const exp = attr.value // oo counter
-			if(this.isDirective(attrName)){
-				const dir = attrName.substring(2)//xx
+  complieElement(node) {
+    //节点是元素
+    //遍历属性列表
+    const nodeAttrs = node.attributes;
+    Array.from(nodeAttrs).forEach((attr) => {
+      // 以k-xx="oo"为例
+      const attrName = attr.name; // k-xx
+      const exp = attr.value; // oo counter
+      if (this.isDirective(attrName)) {
+        const dir = attrName.substring(2); //xx
 
-				//执行指令
-				this[dir] && this[dir](node,exp)
-			}
-		})
-	}
+        //执行指令
+        this[dir] && this[dir](node, exp);
+      }
+    });
+  }
 
-	isDirective(attr){
-		return attr.indexOf('k-') === 0
-	}
-	text(node ,exp){
-		node.textContent = this.$vm[exp]
-	}
-  html(node,exp){
-    node.innerHTML = this.$vm[exp]
+  update(node,exp,dir){
+    // 指令对应的更新函数
+    const fn = this[dir + 'Updater']
+    fn &&fn(node,this.$vm[exp])
+
+
+    //更新处理，封装一个更新选项
+    new Watcher(this.$vm,exp,function (val) {
+      fn && fn(node,val)
+      })
+  }
+
+  textUpdater(node,value){
+    node.textContent = value
+  }
+
+  isDirective(attr) {
+    return attr.indexOf("k-") === 0;
+  }
+  text(node, exp) {
+    // node.textContent = this.$vm[exp];
+    this.update(node,exp,'text')
+  }
+  html(node, exp) {
+    // node.innerHTML = this.$vm[exp];
+    this.update(node,exp,'html')
+  }
+
+  htmlUpdater(node,value){
+    node.innerHTML = value
   }
 }
--- kvue.js
+++ kvue.js
@@ -2,10 +2,14 @@ function defaultReactive(obj, key, val) {
   //递归
   observe(val);
 
+  const dep = new Dep()
+
   //对传入的obj进行传入的拦截
   Object.defineProperty(obj, key, {
     get() {
       console.log("get " + key);
+      //依赖收集
+      Dep.target && dep.addDep(Dep.target)
       return val;
     },
     set(newVal) {
@@ -13,6 +17,10 @@ function defaultReactive(obj, key, val) {
         console.log("set " + key + ":" + newVal);
         observe(newVal);
         val = newVal;
+
+        //通知更新
+        // watchers.forEach((w) => w.update());
+        dep.notify()
       }
     },
   });
@@ -28,32 +36,32 @@ function observe(obj) {
 }
 
 //代理函数方便直接访问$data的内容
-function proxy(vm,sourcekey) {
-  Object.keys(vm[sourcekey]).forEach(key=>{
+function proxy(vm, sourcekey) {
+  Object.keys(vm[sourcekey]).forEach((key) => {
     // 将$data的key代理到vm属性中
-    Object.defineProperty(vm,key,{
-      get(){
-        return vm[sourcekey][key]
-      },set(newVal){
-        vm[sourcekey][key] = newVal
-      }
-    })
-  })
-  }
-
+    Object.defineProperty(vm, key, {
+      get() {
+        return vm[sourcekey][key];
+      },
+      set(newVal) {
+        vm[sourcekey][key] = newVal;
+      },
+    });
+  });
+}
 
 class KVue {
   constructor(options) {
-    this.$options = options
-    this.$data = options.data
+    this.$options = options;
+    this.$data = options.data;
 
-    observe(this.$data)
+    observe(this.$data);
 
     //代理
-    proxy(this,"$data")
+    proxy(this, "$data");
 
     // console.log(typeof this.$options.el);
-    new Compiler(options.el,this)
+    new Compiler(options.el, this);
   }
 }
 
@@ -70,5 +78,44 @@ class Observer {
     Object.keys(obj).forEach((key) => {
       defaultReactive(obj, key, obj[key]);
     });
+  }
+  // 数组数据响应化
+
+}
+
+const watchers = [];
+
+//  观察者
+class Watcher {
+  constructor(vm, key, updateFn) {
+    this.vm = vm;
+    this.key = key;
+    this.updateFn = updateFn;
+
+    // watchers.push(this);
+
+    // Dep。target设置为当前watcher实例
+    Dep.target = this
+    this.vm[this.key]
+    Dep.target = null // 收集完就
+
+  }
+  update() {
+    this.updateFn.call(this.vm, this.vm[this.key]);
+  }
+}
+
+//管理某ge
+class Dep{
+  constructor(){
+    this.deps = []
+  }
+
+  addDep(dep){
+    this.deps.push(dep)
+  }
+
+  notify(){
+    this.deps.forEach(dep=>dep.update())
   }
 }
