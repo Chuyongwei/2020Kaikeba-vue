7967c0fbcd425d198d60c55cf100a38daede624b
wei-elementary <haoyifang@doctor.com>
2021-06-23 11:21:44 +0800

    ts以及vue3的学习01

--- src/test.ts
+++ src/test.ts
@@ -43,3 +43,72 @@ fn2({prop:0})
 type Prop = {prop:number}//这是类
 let Prop = 2//这是变量
 function fn3(o:Prop){}
+
+//类性断言
+const someValue :any = 'this is astrint'
+const strLen = (someValue as string).length
+
+//联合类型
+let union:string |number
+union = '1'
+union=1
+
+//交叉类型
+type First = {first:number}
+type Second = {second:number}
+
+type FirstAndSecond = First&Second
+function fn4():FirstAndSecond{
+	return {first:1,second:2}
+}
+
+//*函数*
+//1. 设置就是必填参数
+//2. 默认值就是msg='abc'
+//3. 可选参数？
+function greeing(person:string,msg1='abc',msg2?:string):string{
+	return ''
+}
+
+greet('tom')
+
+// 函数的重载
+// 先声名，再实现
+//
+function watch(cb1: ()=>void):void;
+function watch(cb1: ()=>void,cb2: (v1:any,v2:any)=>void):void;
+//实现
+function watch(cb1:()=>void,cb2?: (v1:any,v2:any)=>void){
+	if(cb1&&cb2){
+		console.log("执行重载2");
+	}else{
+		console.log("执行重载1");
+		
+	}
+}
+// watch()
+class Parent{
+	private _foo = "foo"
+	protected bar = "bar"
+
+	constructor(public tua = "tua"){}
+
+	private someMethod(){}
+
+	get foo(){
+		return this._foo
+	}
+	set foo(val){
+		this._foo = val;
+	}
+}
+
+const p = new Parent()
+p.foo="fooss"
+class Child extends Parent{
+
+	say(){
+		this.bar
+	}
+}
+const c = new Child()
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
+    <hello/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
+import Hello from '@/components/Hello.vue'; // @ is an alias to /src
 @Component({
   components: {
     HelloWorld,
+    Hello
   },
 })
 export default class Home extends Vue {}
